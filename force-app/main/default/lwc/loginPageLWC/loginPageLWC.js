80cdf285a4e3235d4e74586152b9f0d07497b77b
Your Name <you@example.com>
2022-08-27 00:10:57 +0530

    VsCode new

--- force-app/main/default/lwc/loginPageLWC/loginPageLWC.js
+++ force-app/main/default/lwc/loginPageLWC/loginPageLWC.js
@@ -1,4 +1,7 @@
 import { LightningElement } from 'lwc';
+import CallApex from '@salesforce/apex/Credentials_Class.NewUserDatamethod';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
 
 export default class LoginPageLWC extends LightningElement {
 SHowtheSignup = false;
@@ -25,14 +28,32 @@ this.Email = event.target.value;
 CapturePassword(event){
 this.Password = event.target.value;
 }  
-CreatePassword(){
-    this.CaptureCreatePassword = Event.target.value;
+NewCreatePassword(){
+this.CaptureCreatePassword = Event.target.value;
 }
 CHeckingthevalue(event){
-alert(this.UserName);
-alert(this.MobileNumber);
-alert(this.Email);
-alert(this.Password);
-alert(this.CaptureCreatePassword);
-}              
+alert(this.UserName +'UserName');
+alert(this.MobileNumber +'Mobile');
+alert(this.Email +'Email');
+alert(this.Password +'Password');
+alert(this.CaptureCreatePassword +'NewPassword');
+} 
+DoLogin(){
+CallApex({UName:this.UserName, NewPasscred:this.Password})
+.then()
+.catch(); 
 }
+DoNewCreation(){
+CallApex({ UName: this.UserName, PWD: this.CaptureCreatePassword, EmailID: this.Email, Mob: this.MobileNumber, NewPasscred: this.Password})
+.then(result => {
+const event = new ShowToastEvent({
+title: 'Success!',
+message:  'Your Credentials are Successfully created.',
+variant : 'Success'
+});
+this.dispatchEvent(event);
+})
+.catch(error => {});
+}           
+}
+
