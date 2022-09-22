0d7c0c7e602a4dc48ddd546a8a23d5ed3a2e379a
Bhupalkanakanti <111905935+Bhupalkanakanti@users.noreply.github.com>
2022-09-22 12:21:01 +0530

    New Project

---
+++ force-app/main/default/lwc/forEachLoop/forEachLoop.js
@@ -0,0 +1,25 @@
+import { LightningElement } from 'lwc';
+
+export default class ForEachLoop extends LightningElement {
+
+
+    CarList =["Audi","Ferrari","Scorpio","Toyato","Verna","Maruthi","Hyundai"];
+    ProgramList=[
+        {
+Id :"12",
+Language :"HTML"
+        },
+        {
+            Id :"13",
+            Language :"Python"
+        },
+        {
+            Id :"14",
+            Language :"C++"
+        },
+        {
+            Id :"15",
+            Language :"Ruby"
+        }
+    ]
+}
