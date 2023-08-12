b932c278857448286bfacdf214ac39de470c25f6
Bhupalkanakanti <111905935+Bhupalkanakanti@users.noreply.github.com>
2023-08-12 18:16:06 +0530

    latest

--- force-app/main/default/lwc/loginPageLWC/loginPageLWC.js
+++ force-app/main/default/lwc/loginPageLWC/loginPageLWC.js
@@ -1,92 +1,92 @@
-import { LightningElement } from 'lwc';
-import CallApex from '@salesforce/apex/Credentials_Class.NewUserDatamethod';
-import Validatecred from '@salesforce/apex/Credentials_Class.Validateuers';
-import { NavigationMixin } from 'lightning/navigation';
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-export default class LoginPageLWC extends NavigationMixin (LightningElement) {
-SHowtheSignup = false;
-UserName;
-UserName1;
-CaptureCreatePassword;
-Email;
-Password;
-DoSignup(){
-this.SHowtheSignup = true;
-}
-DoLoginFirst(){
-this.SHowtheSignup = false;
-}
-CaptureUserName(event){
-this.UserName = event.detail.value;
-}
-CaptureUserName1(event){
-this.UserName1 = event.detail.value;
-} 
-CaptureEmail(event){
-this.Email = event.detail.value;
-} 
-CapturePassword(event){
-this.Password = event.detail.value;
-}
-NewCreatePassword(event){
-this.CaptureCreatePassword = event.detail.value;
-}
+import { LightningElement } from 'lwc'
+import CallApex from '@salesforce/apex/Credentials_Class.NewUserDatamethod'
+import Validatecred from '@salesforce/apex/Credentials_Class.Validateuers'
+import { NavigationMixin } from 'lightning/navigation'
+import { ShowToastEvent } from 'lightning/platformShowToastEvent'
+export default class LoginPageLWC extends NavigationMixin(LightningElement) {
+  SHowtheSignup = false
+  UserName
+  UserName1
+  CaptureCreatePassword
+  Email
+  Password
+  DoSignup () {
+    this.SHowtheSignup = true
+  }
+  DoLoginFirst () {
+    this.SHowtheSignup = false
+  }
+  CaptureUserName (event) {
+    this.UserName = event.detail.value
+  }
+  CaptureUserName1 (event) {
+    this.UserName1 = event.detail.value
+  }
+  CaptureEmail (event) {
+    this.Email = event.detail.value
+  }
+  CapturePassword (event) {
+    this.Password = event.detail.value
+  }
+  NewCreatePassword (event) {
+    this.CaptureCreatePassword = event.detail.value
+  }
 
-DoNewCreation(){
-CallApex({ UName:this.UserName, PWD:this.CaptureCreatePassword, EmailID:this.Email})
-.then( ()=> {
-const event = new ShowToastEvent({
-title: 'Success',
-message:
-'This credential details are created successfully.',
-variant:'Success',
-});
-this.dispatchEvent(event);
-});
-// eslint-disable-next-line vars-on-top
-var CompoDefinition = {
-    componentDef : "c:navigationLwc",
-    attributes : {
-        FullName : this.UserName,
-    }
-    };
-    // eslint-disable-next-line vars-on-top
-    var encodedComDef = btoa (JSON.stringify(CompoDefinition));
-    this[NavigationMixin.Navigate]({
-    type:"standard__webPage",
-    attributes : {
-    url : '/one/one.app#' + encodedComDef
-    }
+  DoNewCreation () {
+    CallApex({
+      UName: this.UserName,
+      PWD: this.CaptureCreatePassword,
+      EmailID: this.Email
+    }).then(() => {
+      const event = new ShowToastEvent({
+        title: 'Success',
+        message: 'This credential details are created successfully.',
+        variant: 'Success'
+      })
+      this.dispatchEvent(event)
     })
-.catch(() => {});
-}
-DoLogin(){
-Validatecred({UserNameLog :this.UserName1, PasswordLog :this.Password})
-.then(result => {
-    if(result && result.length>0){
-const event = new ShowToastEvent({
-title: 'Success',
-message:
-'This credential details are Matched successfully.',
-variant:'Success',
-});
-this.dispatchEvent(event);
     // eslint-disable-next-line vars-on-top
     var CompoDefinition = {
-        componentDef : "c:navigationLwc",
-        attributes : {
-            FullName : this.UserName1,
-        }
-        };
-        // eslint-disable-next-line vars-on-top
-        var encodedComDef = btoa (JSON.stringify(CompoDefinition));
-        this[NavigationMixin.Navigate]({
-        type:"standard__webPage",
-        attributes : {
-        url : '/one/one.app#' + encodedComDef
-        }
-        });
-        /*
+      componentDef: 'c:navigationLwc',
+      attributes: {
+        FullName: this.UserName
+      }
+    }
+    // eslint-disable-next-line vars-on-top
+    var encodedComDef = btoa(JSON.stringify(CompoDefinition))
+    this[NavigationMixin.Navigate]({
+      type: 'standard__webPage',
+      attributes: {
+        url: '/one/one.app#' + encodedComDef
+      }
+    }).catch(() => {})
+  }
+  DoLogin () {
+    Validatecred({ UserNameLog: this.UserName1, PasswordLog: this.Password })
+      .then(result => {
+        if (result && result.length > 0) {
+          const event = new ShowToastEvent({
+            title: 'Success',
+            message: 'This credential details are Matched successfully.',
+            variant: 'Success'
+          })
+          this.dispatchEvent(event)
+          // eslint-disable-next-line vars-on-top
+          var CompoDefinition = {
+            componentDef: 'c:navigationLwc',
+            attributes: {
+              FullName: this.UserName1
+            }
+          }
+          // eslint-disable-next-line vars-on-top
+          var encodedComDef = btoa(JSON.stringify(CompoDefinition))
+          this[NavigationMixin.Navigate]({
+            type: 'standard__webPage',
+            attributes: {
+              url: '/one/one.app#' + encodedComDef
+            }
+          })
+          /*
         this[NavigationMixin.Navigate]({
             type: 'standard__objectPage',
             attributes: {
@@ -94,16 +94,15 @@ this.dispatchEvent(event);
                 actionName: 'home',
             },
         });*/
-}else{
-    const event = new ShowToastEvent({
-        title: 'Failed',
-        message:
-        'This credential details are not Matched!!.',
-        variant:'Error',
-        });
-        this.dispatchEvent(event);
-}
-})
-.catch();
-}        
+        } else {
+          const event = new ShowToastEvent({
+            title: 'Failed',
+            message: 'This credential details are not Matched!!.',
+            variant: 'Error'
+          })
+          this.dispatchEvent(event)
+        }
+      })
+      .catch()
+  }
 }
--- force-app/main/default/lwc/selector/selector.js
+++ force-app/main/default/lwc/selector/selector.js
@@ -1,26 +1,32 @@
-import { LightningElement, wire } from 'lwc';
-import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import { LightningElement, wire, api } from 'lwc'
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi'
 
-import USER_NAME from '@salesforce/schema/User.Username';
-import FIRST_NAME from '@salesforce/schema/User.FirstName';
-import LAST_NAME from '@salesforce/schema/User.LastName';
+import USER_NAME from '@salesforce/schema/User.Username'
+import FIRST_NAME from '@salesforce/schema/User.FirstName'
+import LAST_NAME from '@salesforce/schema/User.LastName'
 
-import USER_ID from '@salesforce/user/Id';
+import USER_ID from '@salesforce/user/Id'
 export default class UserInfo extends LightningElement {
-    userId = USER_ID;
-    
-    @wire(getRecord, { recordId : "$userId", fields: [USER_NAME, FIRST_NAME, LAST_NAME]})
-    user;
+  userId = USER_ID
+  @api first = 'Bhupal'
+  @api imagedecision = false
+  @api title = 'first Title'
+  @api imageUrl = ''
+  @wire(getRecord, {
+    recordId: '$userId',
+    fields: [USER_NAME, FIRST_NAME, LAST_NAME]
+  })
+  user
 
-    get userName(){
-        return getFieldValue(this.user.data, USER_NAME);
-    }
+  get userName () {
+    return getFieldValue(this.user.data, USER_NAME)
+  }
 
-    get firstName(){
-        return getFieldValue(this.user.data, FIRST_NAME);
-    }
+  get firstName () {
+    return getFieldValue(this.user.data, FIRST_NAME)
+  }
 
-    get lastName(){
-        return getFieldValue(this.user.data, LAST_NAME);
-    }
+  get lastName () {
+    return getFieldValue(this.user.data, LAST_NAME)
+  }
 }
