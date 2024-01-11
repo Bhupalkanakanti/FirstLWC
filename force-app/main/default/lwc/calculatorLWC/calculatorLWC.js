92c34196ba857f164eba08885accbc1794b7f786
Bhupal Kanakanti <bhupal1.sfdc@gmail.com>
2024-01-11 21:29:08 +0530

    js push

--- force-app/main/default/lwc/calculatorLWC/calculatorLWC.js
+++ force-app/main/default/lwc/calculatorLWC/calculatorLWC.js
@@ -1,16 +1,17 @@
 import { LightningElement, track } from 'lwc'
 
 export default class CalculatorLWC extends LightningElement {
-  @track first
-  @track Second
-  @track result
-  FirstNumber (event) {
-    this.first = event.target.value
-  }
-  SecondNumber (event) {
-    this.Second = event.target.value
-  }
-  SumofNumbers () {
-    this.result = Number(this.first) + Number(this.Second)
-  }
+    //Track is withdrawn from the salesforce for bi directional data binding.
+    @track first
+    @track Second
+    @track result
+    FirstNumber(event) {
+        this.first = event.target.value
+    }
+    SecondNumber(event) {
+        this.Second = event.target.value
+    }
+    SumofNumbers() {
+        this.result = Number(this.first) + Number(this.Second)
+    }
 }
