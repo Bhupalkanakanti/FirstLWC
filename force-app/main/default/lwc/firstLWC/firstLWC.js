91631786a5910486a7b6409e35bd15e3b9fb9c4f
Bhupal@TR <bhupal.k@targetrecruit.net>
2024-10-20 19:34:45 +0530

    new

--- force-app/main/default/lwc/firstLWC/firstLWC.js
+++ force-app/main/default/lwc/firstLWC/firstLWC.js
@@ -1,9 +1,10 @@
-import { LightningElement,api} from 'lwc';
+import { LightningElement, api } from 'lwc';
 import Usrd from '@salesforce/user/Id';
 
 export default class firstLWC extends LightningElement {
- @api recordId;
- @api accountId;
- @api objectApiName;
-     Userrr = Usrd;    
- }
+    @api recordId;
+    @api accountId;
+    @api objectApiName;
+    Userrr = Usrd;
+
+}
